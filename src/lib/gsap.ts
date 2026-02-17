88198e84473da8f27095a96e6c0b40d2c958c2ec
JoshTabsDEv <tabura.joshua10@gmail.com>
2026-02-17 09:34:02 +0800

    fix: remove unused GSAP plugins from registration

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -3,10 +3,8 @@ import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollSmoother } from 'gsap/ScrollSmoother';
 import { SplitText } from 'gsap/SplitText';
-import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
-import { Observer } from 'gsap/Observer';
 
 // Register all plugins once
-gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText, DrawSVGPlugin, Observer);
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, SplitText);
 
-export { gsap, useGSAP, ScrollTrigger, ScrollSmoother, SplitText, DrawSVGPlugin, Observer };
+export { gsap, useGSAP, ScrollTrigger, ScrollSmoother, SplitText };
